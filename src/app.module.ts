43bd19f11a35d76e7d8228cf77fec1af05cbade2
EngrMuqeetAhmad <hahmadqayyum@gmail.com>
2025-12-30 17:37:15 +0500

    initialized nest project, setup sequelize, config module, validation pipeline

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'patient_agent',
+      autoLoadModels: true,
+      models: [],
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+      logging: true,
+      sync: {
+        force: false,
+        alter: process.env.NODE_ENV === 'development',
+      },
+      retryDelay: 3000,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
